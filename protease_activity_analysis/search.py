b3719a92ce11ee84eae536abe9474b2d25a76e77
Carmen Martin <carmenmartinbsc@gmail.com>
2020-05-27 21:42:00 -0400

    Added functions to search.py

--- protease_activity_analysis/search.py
+++ protease_activity_analysis/search.py
@@ -1,5 +1,8 @@
 """ Collection of data searching/lookup functions based on existing screening data"""
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from scipy.special import ndtr
 
 # for future: consider adapting to being able to take a list of  proteases or to not take a path
 def search_protease(prot, screen_names, path):
@@ -107,3 +110,122 @@ def make_logo(substrate_sequences, title, logo_format, size):
     """
 
     raise NotImplementedError
+
+
+# for future: consider changing format of image or making it an argument + adding defaults + figure out bin width
+def plot_distribution(screen_data, n_rows, n_cols, fig_size, col, title, x_label):
+    """ Make subplot of screening data distributions (e.g. fold changes or z-scores)
+
+    Args:
+        screen_data (pandas.df): screening data with df.columns = proteases  and df.index=Substrates
+        nrows (int): number of rows in subplot
+        ncols (int): number of columns in subplot
+        fig_size (tuple, int): fize of figure - e.g. '(width,height)' in inches
+        col (str): color of plots e.g. 'g', r', 'b'
+        title (str): title of subplot
+        x_label (str): label of x-axis
+
+    Returns:
+        png: Subplot of distributions
+    """
+    # Plot distribution of fold changes
+    fig, axes = plt.subplots(nrows=n_rows, ncols=n_cols, figsize=fig_size)
+    fig.subplots_adjust(hspace=0.5)
+    fig.subplots_adjust(wspace=0.5)
+    sns.set(style="white", palette="muted", color_codes=True)
+
+    for ax, feature in zip(axes.flatten(), screen_data):
+        f = sns.distplot(screen_data[feature], ax=ax, color=col, axlabel=False,
+                         kde=False)  # bins=len(np.unique(screen_data.T.iloc[0]))//2, color="g", axlabel = False)
+        ax.set(title=feature)
+
+    fig.suptitle(title)
+    fig.text(0.5, 0.04, x_label, ha='center', va='center')
+    fig.text(0.06, 0.5, 'Frequency', ha='center', va='center', rotation='vertical')
+    fig.show()
+
+    # plt.savefig(out_path, dpi=300)
+
+    return
+
+
+def plot_std(screen_data, n_bins=30):
+    """ Compute std of proteases in raw data to threshold for inactivity
+
+    Args:
+        screen_data (pandas.df): screening data with df.columns = proteases  and df.index=Substrates
+        n_bins (int): number of bins for histogram
+    Returns:
+        matplotlib figure: Histogram of std of raw data
+        std_data (list): list of std of each column in the dataframe
+    """
+    # Compute std of proteases in raw data to identify those that did not cleave anything
+    std_data = []
+
+    for col in screen_data.columns:
+        std_data.append(screen_data[col].std(ddof=0))
+
+    plt.plot(figzise=(6, 4))
+    plt.hist(std_data, bins=n_bins)
+    plt.title('Histogram of Std of raw data')
+    plt.xlabel('Raw Data Std')
+    plt.ylabel('Frequency')
+    plt.show()
+    # plt.savefig('outputs/histogram_std_raw.png', dpi=300)
+
+    # plt.savefig('outputs/histogram_std_raw.png', dpi=300)
+
+    return std_data
+
+
+def subset_active_inactive(screen_data, std_data, std_threshold):
+    """ Subset screening data into active and inactive proteases
+
+    Args:
+        screen_data (pandas.df): screening data with df.columns = proteases  and df.index=Substrates
+        std_data (list, float): list of std of each column in the dataframe, columns od screen_data nd std_data must match
+        std_threshold (float): std threshold used for subsetting
+    Returns:
+        active_data (pandas.df): screening data from active proteases, above user set std threshold
+        inactive_data (pandas.df): screening data from inactive proteases, below user set std threshold
+    """
+    # Add Std to dataframe of screen data
+    std_data_df = pd.DataFrame({'Std': std_data}, index=screen_data.columns)
+    screen_data = screen_data.append(std_data_df.T)
+
+    # Subset into active and inactive
+    active_data = screen_data.loc[:, screen_data.loc['Std'] > std_threshold]
+    inactive_data = screen_data.loc[:, screen_data.loc['Std'] < std_threshold]
+
+    return active_data, inactive_data
+
+
+def calculate_zscore_pval(screen_data):
+    """ Calculate z score by protease and calculate statistical significance by converting
+    Z scores into p values by 1 - pnorm (Z value)
+
+    Args:
+        screen_data (pandas.df): screening data with df.columns = proteases  and df.index=Substrates
+
+    Returns:
+        screen_zscore (pandas.df): z-scored data
+        screen_pval (pandas.df): p-values for data (note, not adjusted for mutiple hypothesis testing)
+        min_pval (list, float): minimum pvalue for each protease
+    """
+    # Calculate Z score for only active
+    first_col = screen_data.columns[0]
+    dic = {first_col: []}
+    screen_zscore = pd.DataFrame(dic)
+
+    for col in screen_data.columns:
+        screen_zscore[col] = ((screen_data[col] - screen_data[col].mean()) / screen_data[col].std(ddof=0))
+
+    # Calculate p value of zscores for only active, keep a record of minimum p value fopr each protease
+    screen_pval = pd.DataFrame(dic)
+    min_pval = []
+
+    for col in screen_zscore.columns:
+        screen_pval[col] = 1 - ndtr(screen_zscore[col])
+        min_pval.append(min(screen_pval[col]))
+
+    return screen_zscore, screen_pval, min_pval
