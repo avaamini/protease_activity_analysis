f66c2c34c0410e8906e92c6c23ce50f090a1cfc4
Melodi Anahtar <melodi@Melodis-MacBook-Pro.local>
2020-06-08 18:16:08 -0400

    added 20-plex

--- test_data_normalization.py
+++ test_data_normalization.py
@@ -12,21 +12,26 @@ stock_name = "Stock"
 
 syneos_data = paa.data.load_syneos(data_path, id_path, sheets, stock_name)
 
-# print(syneos_data)
-
-plex_14 = ["1UR3_01", "2UR3_02", "3UR3_03", "4UR3_04", "5UR3_05", "6UR3_06", 
+# 14-plex
+plex = ["1UR3_01", "2UR3_02", "3UR3_03", "4UR3_04", "5UR3_05", "6UR3_06", 
            "7UR3_07", "8UR3_08", "9UR3_10", "1UR3_11", "2UR3_13", "3UR3_15", 
            "4UR3_16", "5UR3_18"]
 
-plex_20 = []
+# 20-plex
+# plex= ["1UR3_01", "2UR3_02", "3UR3_03", "4UR3_04", "5UR3_05", "6UR3_06", 
+#            "7UR3_07","8UR3_08", "9UR3_10", "1UR3_11", "2UR3_13", "3UR3_15", 
+#            "4UR3_16", "5UR3_18", "6UR3_20", "1UR3_09", "2UR3_12", "3UR3_14", 
+#            "4UR3_17", "5UR3_19"]
 
-normalized_matrix = paa.data.process_syneos_data(syneos_data, plex_14)
+syneos_data = syneos_data.drop(stock_name, level='Sample ID')
 
-normalized_matrix.index = syneos_data.index[:-1]
-normalized_matrix.columns = plex_14
+normalized_matrix = paa.data.process_syneos_data(syneos_data, plex)
+
+normalized_matrix.index = syneos_data.index
+normalized_matrix.columns = plex
 
 """ to make a heatmap """
-update = paa.vis.plot_heatmap(normalized_matrix, plex_14)
+update = paa.vis.plot_heatmap(normalized_matrix, plex)
   
 """ to perform PCA """
 undo_multiindex = normalized_matrix.reset_index()
@@ -34,7 +39,7 @@ undo_multiindex = normalized_matrix.reset_index()
 #if you want to filter for only some Sample Types, change code below
 #undo_multiindex = undo_multiindex [~undo_multiindex['Sample Type'].str.contains("LAM")]
   
-pca = paa.vis.plot_pca(undo_multiindex, plex_14)
+pca = paa.vis.plot_pca(undo_multiindex, plex)
 
 """ """
 
