dbbd7b3b932fc8ab154eb19c91ab9cf7dfaacec5
Ava Soleimany <avasoleimany@dhcp-18-20-179-20.dyn.mit.edu>
2020-07-09 11:21:54 -0400

    moving relevant data files for demo to outer tests folder

---
+++ protease_activity_analysis/utils.py
@@ -0,0 +1,20 @@
+""" Utilities for analysis functions. """
+
+import numpy as np
+import pandas as pd
+import itertools
+
+## Global variables: reporter names for 14 and 20 plex MS panels
+PLEX_14 = ["1UR3_01", "2UR3_02", "3UR3_03", "4UR3_04", "5UR3_05", "6UR3_06",
+       "7UR3_07", "8UR3_08", "9UR3_10", "1UR3_11", "2UR3_13", "3UR3_15",
+       "4UR3_16", "5UR3_18"]
+RENAMED_14 = ["PP01", "PP02", "PP03", "PP04", "PP05", "PP06", "PP07", "PP08",
+          "PP09", "PP10", "PP11", "PP12", "PP13", "PP14"]
+
+PLEX_20 = ["1UR3_01", "2UR3_02", "3UR3_03", "4UR3_04", "5UR3_05", "6UR3_06",
+            "7UR3_07","8UR3_08", "9UR3_10", "1UR3_11", "2UR3_13", "3UR3_15",
+            "4UR3_16", "5UR3_18", "6UR3_20", "1UR3_09", "2UR3_12", "3UR3_14",
+            "4UR3_17", "5UR3_19"]
+RENAMED_20 = ["PP01", "PP02", "PP03", "PP04", "PP05", "PP06", "PP07", "PP08",
+              "PP09", "PP10", "PP11", "PP12", "PP13", "PP14", "PP15", "PP16",
+              "PP17", "PP18", "PP19", "PP20"]
