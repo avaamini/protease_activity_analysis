73f0749d22e956f74bfd27c108deb17f8a9d2382
Ava Soleimany <apsoleimany@gmail.com>
2022-02-28 14:06:15 -0500

    cleaning up and finalizing protease.py

--- protease_activity_analysis/protease.py
+++ protease_activity_analysis/protease.py
@@ -1,16 +1,15 @@
-""" Protease-related functions"""
+""" Protease-related helper functions."""
 import pickle
 import pandas as pd
-
 
 def classify_protease(protease):
     """ Map human protease name to protease class
 
     Args:
-        prot (str): name of human protease to be mapped
+        protease (str): name of human protease to be mapped
 
     Returns:
-        class (str): class of protease
+        class_mem (str): class membership of query protease
     """
 
     f = open('data/screens/PAA/protease_class_dict.pkl', 'rb')
@@ -19,7 +18,6 @@ def classify_protease(protease):
     class_mem = None
 
     for key in class_dict.keys():
-        # print(key)
         if protease in class_dict[key]:
             class_mem = key
     if class_mem is None:
@@ -30,7 +28,9 @@ def classify_protease(protease):
 
 
 def species_to_species(species_1, species_2, protease):
-    """ Map protease from one species to another. Species include Human, Mouse, Chimpanzee and Raet. source: http://degradome.uniovi.es/
+    """ Map protease name from one species to another.
+    Species include Human, Mouse, Chimpanzee, Rat.
+    Source: http://degradome.uniovi.es/
 
     Args:
         species_1 (str): original species
@@ -38,16 +38,19 @@ def species_to_species(species_1, species_2, protease):
         protease (str): protease to be mapped from species_1 to species_2
 
     Returns:
-        mapped_protease (str): mapped protease
+        mapped_protease (str): protease homolog in the new species (species_2)
+    Raises:
+        KeyError: if query protease is not valid for species_1
     """
     data_path = 'data/screens/PAA/Species_map.xlsx'
     spec_map = pd.read_excel(data_path, header=0)
 
     keys = spec_map[species_1].to_list()
     vals = spec_map[species_2].to_list()
-    spec1_to_seqc2 = dict(zip(keys, vals))
+    species1_to_species2 = dict(zip(keys, vals))
 
-    if protease in spec1_to_seqc2.keys():
-        return spec1_to_seqc2[protease]
+    if protease in species1_to_species2.keys():
+        mapped_protease = species1_to_species2[protease]
+        return mapped_protease
     else:
-        print('Please enter valid protease for ' + species_1)
+        raise KeyError('Please enter valid protease for ' + species_1)
